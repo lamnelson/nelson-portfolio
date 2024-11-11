4dee6f40ea6d855c80404d3da866ae05a3c7bacc
Nelson Lam <nelsonlam02@gmail.com>
2024-11-10 18:25:06 -0600

    Update HeroImage.tsx

--- src/components/features/home/components/Hero/components/HeroImage.tsx
+++ src/components/features/home/components/Hero/components/HeroImage.tsx
@@ -7,7 +7,7 @@ export const HeroImage = memo(function HeroImage() {
 	return (
 		<Image
 			alt="Picture of the author"
-			className="m-auto mb-8 h-auto max-h-[800px] rounded-xl w-full max-w-[300px] max-w-md justify-center object-cover object-center lg:m-auto lg:max-w-xl"
+			className="m-auto mb-8 h-auto max-h-[800px] rounded-xl w-full max-w-[200px] max-w-md justify-center object-cover object-center lg:m-auto lg:max-w-xl"
 			draggable={false}
 			height={800}
 			priority
